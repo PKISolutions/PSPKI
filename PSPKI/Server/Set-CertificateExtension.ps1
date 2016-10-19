b07bde0d6ff553d0f69e88fa306ce0af4e5d38e1
Vadims Podans <bokorezi@inbox.lv>
2016-10-19 23:45:12 +0300

    Fixed issue: https://pspki.codeplex.com/workitem/101

--- PSPKI/Server/Set-CertificateExtension.ps1
+++ PSPKI/Server/Set-CertificateExtension.ps1
@@ -33,7 +33,7 @@
 			} elseif ($Extension[0] -is  [Security.Cryptography.X509Certificates.X509Extension] -and !$Remove) {
 				foreach ($ext in $Extension) {
 					$ext = [Security.Cryptography.X509Certificates.X509Extension]$ext
-					[Byte[]]$bytes = if ($ext.RawData % 2) {$ext.RawData + 0} else {$ext.RawData}
+					[Byte[]]$bytes = if ($ext.RawData.Length % 2) {$ext.RawData + 0} else {$ext.RawData}
 					$derValue = [Text.Encoding]::Unicode.GetString($bytes)
 					try {
 						$CertAdmin.SetCertificateExtension($Req.ConfigString,$Req.RequestID,$ext.Oid.Value,0x3,$ext.Critical,$derValue)
@@ -50,7 +50,7 @@
 				foreach ($ext in $Extension) {
 					try {
 						$oid = New-Object Security.Cryptography.Oid $ext
-						[void][PKI.ASN.ASN1]::EncodeObjectIdentifier($ext)
+						[void][SysadminsLV.Asn1Parser.Asn1Utils]::EncodeObjectIdentifier($ext)
 						$CertAdmin.SetCertificateExtension($Req.ConfigString,$Req.RequestID,$oid.Value,0x1,0x2,0)
 						Write-Verbose "Extension OID='$($oid.Value)' was removed from request ID='$($Req.RequestID)'."
 					} catch {
