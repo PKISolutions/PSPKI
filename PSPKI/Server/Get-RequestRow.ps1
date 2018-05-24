1c33a48ec37a3c1f13094a8b85c5b60db5dc69a6
crypt32 <vpodans@sysadmins.lv>
2018-05-24 18:12:32 +0300

    Fixed issue: https://github.com/Crypt32/PSPKI/issues/34

--- PSPKI/Server/Get-RequestRow.ps1
+++ PSPKI/Server/Get-RequestRow.ps1
@@ -26,13 +26,13 @@ function __handleOidColumns($Row) {
     $RColumn = $CaView.GetColumnIndex(0, "Disposition")
 # set proper table
     switch ($Table) {
-        "Revoked"	{$CaView.SetRestriction($RColumn,1,0,21)}
-        "Issued"	{$CaView.SetRestriction($RColumn,1,0,20)}
-        "Pending"	{$CaView.SetRestriction($RColumn,1,0,9)}
-        "Failed"	{$CaView.SetRestriction(-3,0,0,0)}
-        "Extension"	{$CaView.SetTable(0x3000)}
-        "Attribute"	{$CaView.SetTable(0x4000)}
-        "CRL"		{$CaView.SetTable(0x5000)}
+        "Revoked"   {$CaView.SetRestriction($RColumn,1,0,21)}
+        "Issued"    {$CaView.SetRestriction($RColumn,1,0,20)}
+        "Pending"   {$CaView.SetRestriction($RColumn,1,0,9)}
+        "Failed"    {$CaView.SetRestriction(-3,0,0,0)}
+        "Extension" {$CaView.SetTable(0x3000)}
+        "Attribute" {$CaView.SetTable(0x4000)}
+        "CRL"       {$CaView.SetTable(0x5000)}
     }
 # parse restriction filters
     if ($Filter -ne $null) {
@@ -57,9 +57,9 @@ function __handleOidColumns($Row) {
             $Value = $matches[3]
             $SchemaRow = $Schema | Where-Object {$_.Name -eq $matches[1]}
             $Value = switch ([int]$SchemaRow.DataType) {
-                1		{$matches[3] -as [int]}
-                2		{[DateTime]::ParseExact($matches[3],"MM/dd/yyyy HH:mm:ss",[Globalization.CultureInfo]::InvariantCulture)}
-                default	{$matches[3]}
+                1        {$matches[3] -as [int]}
+                2        {[DateTime]::ParseExact($matches[3],"MM/dd/yyyy HH:mm:ss",[Globalization.CultureInfo]::InvariantCulture)}
+                default  {$matches[3]}
             }
             if ($matches[1] -eq "CertificateTemplate") {
                 if ($Value -ne "Machine") {
@@ -82,14 +82,14 @@ function __handleOidColumns($Row) {
         0..($ColumnCount - 1) | ForEach-Object {$CaView.SetResultColumn($_)}
     } else {
         $properties = switch ($Table) {
-            "Revoked"	{"RequestID","Request.RevokedWhen","Request.RevokedReason","CommonName","SerialNumber","CertificateTemplate"}
-            "Issued"	{"RequestID","Request.RequesterName","CommonName","NotBefore","NotAfter","SerialNumber","CertificateTemplate"}
-            "Pending"	{"RequestID","Request.RequesterName","Request.SubmittedWhen","Request.CommonName","CertificateTemplate"}
-            "Failed"	{"RequestID","Request.StatusCode","Request.DispositionMessage","Request.SubmittedWhen","Request.CommonName","CertificateTemplate"}
-            "Request"	{"RequestID","Request.StatusCode","Request.DispositionMessage","Request.RequesterName","Request.SubmittedWhen","Request.CommonName","CertificateTemplate"}
+            "Revoked"   {"RequestID","Request.RevokedWhen","Request.RevokedReason","CommonName","SerialNumber","CertificateTemplate"}
+            "Issued"    {"RequestID","Request.RequesterName","CommonName","NotBefore","NotAfter","SerialNumber","CertificateTemplate"}
+            "Pending"   {"RequestID","Request.RequesterName","Request.SubmittedWhen","Request.CommonName","CertificateTemplate"}
+            "Failed"    {"RequestID","Request.StatusCode","Request.DispositionMessage","Request.SubmittedWhen","Request.CommonName","CertificateTemplate"}
+            "Request"   {"RequestID","Request.StatusCode","Request.DispositionMessage","Request.RequesterName","Request.SubmittedWhen","Request.CommonName","CertificateTemplate"}
             "Extension" {"ExtensionRequestId","ExtensionName","ExtensionFlags","ExtensionRawValue"}
             "Attribute" {"AttributeRequestId","AttributeName","AttributeValue"}
-            "CRL"		{"CRLRowId","CRLNumber","CRLThisUpdate","CRLNextUpdate","CRLPublishStatusCode","CRLPublishError"}
+            "CRL"       {"CRLRowId","CRLNumber","CRLThisUpdate","CRLNextUpdate","CRLPublishStatusCode","CRLPublishError"}
         }
         $properties = $properties + $Property | Select-Object -Unique | Where-Object {$_}
         $CaView.SetResultColumnCount($properties.Count)
@@ -101,19 +101,19 @@ function __handleOidColumns($Row) {
         $Row = New-Object PKI.CertificateServices.DB.RequestRow
         $Row.ConfigString = $CA.ConfigString
         $Row.Table = switch ($Table) {
-            "Extension"	{[PKI.CertificateServices.DB.TableList]::Extension}			
-            "Attribute"	{[PKI.CertificateServices.DB.TableList]::Attribute}
-            "CRL"		{[PKI.CertificateServices.DB.TableList]::CRL}
-            default		{[PKI.CertificateServices.DB.TableList]::Request}
+            "Extension"    {[PKI.CertificateServices.DB.TableList]::Extension}            
+            "Attribute"    {[PKI.CertificateServices.DB.TableList]::Attribute}
+            "CRL"          {[PKI.CertificateServices.DB.TableList]::CRL}
+            default        {[PKI.CertificateServices.DB.TableList]::Request}
         }
         $DbColumn = $DbRow.EnumCertViewColumn()
         while ($DbColumn.Next() -ne -1) {
             $colName = $DbColumn.GetName()
             $colVal = $DbColumn.GetValue(1)
             if (
-                $colName -eq "RequestID"			-or
-                $colName -eq "ExtensionRequestId"	-or
-                $colName -eq "AttributeRequestId"	-or
+                $colName -eq "RequestID"            -or
+                $colName -eq "ExtensionRequestId"   -or
+                $colName -eq "AttributeRequestId"   -or
                 $colName -eq "CRLRowId"
             ) {
                 $Row.RowId = $colVal
@@ -123,10 +123,8 @@ function __handleOidColumns($Row) {
             $Row | Add-Member -MemberType NoteProperty $colName -Value $colVal -Force            
         }
         if ($Row.CertificateTemplate -match "^(\d+\.){3}") {
-            if ([string]::IsNullOrEmpty(([Security.Cryptography.Oid]$colVal).FriendlyName)) {
-                $Row.CertificateTemplate = $colVal
-            } else {
-                $Row.CertificateTemplate = ([Security.Cryptography.Oid]$colVal).FriendlyName
+            if (![string]::IsNullOrEmpty(([Security.Cryptography.Oid]$Row.CertificateTemplate).FriendlyName)) {
+                $Row.CertificateTemplate = ([Security.Cryptography.Oid]$Row.CertificateTemplate).FriendlyName
             }
         }
         __handleOidColumns $Row
