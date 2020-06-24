624a83b9296459602e8c095f118fd6489ce81668
Vadims Podans <vpodans@sysadmins.lv>
2020-06-24 16:11:20 +0300

    implemented -Enterprise and -Standalone filters

--- PSPKI/Server/Get-CertificationAuthority.ps1
+++ PSPKI/Server/Get-CertificationAuthority.ps1
@@ -3,15 +3,47 @@
 .ExternalHelp PSPKI.Help.xml
 #>
 [OutputType('PKI.CertificateServices.CertificateAuthority[]')]
-[CmdletBinding(DefaultParameterSetName = '__ComputerSet')]
-	param(
-		[Parameter(ParameterSetName = "__ComputerSet", Position = 0)]
-		[string]$ComputerName = "*",
-		[Parameter(ParameterSetName = "__NameSet", Position = 0)]
-		[string]$Name = "*"
-	)
-	switch ($PsCmdlet.ParameterSetName) {
-		"__ComputerSet" {[PKI.CertificateServices.CertificateAuthority]::EnumEnterpriseCAs("Server",$ComputerName)}
-		"__NameSet" {[PKI.CertificateServices.CertificateAuthority]::EnumEnterpriseCAs("Name",$Name)}
-	}
+[CmdletBinding(DefaultParameterSetName = '__computer')]
+    param(
+        [Parameter(ParameterSetName = "__computer", Position = 0)]
+        [string]$ComputerName = "*",
+        [Parameter(ParameterSetName = "__name", Position = 0)]
+        [string]$Name = "*",
+        [switch]$Enterprise,
+        [switch]$Standalone
+    )
+
+    $DSList = @{}
+    if (!$NoDomain) {
+        $DSList = [SysadminsLV.PKI.Management.ActiveDirectory.DsPkiContainer]::GetAdPkiContainer("Enroll").EnrollmentServers
+    } elseif ($Enterprise -and !$Standalone) {
+        # at this point we are in workgroup and only enterprise CAs were requested. There is nothing to return.
+        return
+    }
+
+    $CertConfig = New-Object SysadminsLV.PKI.Dcom.Implementations.CertConfigD
+    $CertSrvList = $CertConfig.EnumConfigEntries()
+
+    # filter by name
+    $FinalList = switch ($PsCmdlet.ParameterSetName) {
+        '__computer' {$CertSrvList | Where-Object {$_.ComputerName -like $ComputerName}}
+        '__name' {$CertSrvList | Where-Object {$_.CommonName -like $Name}}
+    }
+
+    # filter by Enterprise/Standalone
+    if ($PSBoundParameters.ContainsKey("Enterprise") -or $PSBoundParameters.ContainsKey("Standalone")) {
+        if ($PSBoundParameters.ContainsKey("Enterprise") -and !$PSBoundParameters.ContainsKey("Standalone")) {
+            $tempList = $DSList | Where-Object {($_.Flags -band [SysadminsLV.PKI.Management.ActiveDirectory.DsEnrollServerFlag]::NoTemplateSupport) -eq 0}
+        } elseif (!$PSBoundParameters.ContainsKey("Enterprise") -and $PSBoundParameters.ContainsKey("Standalone")) {
+            $tempList = $DSList | Where-Object {($_.Flags -band [SysadminsLV.PKI.Management.ActiveDirectory.DsEnrollServerFlag]::NoTemplateSupport) -gt 0}
+        } else {
+            $tempList = $DSList
+        }
+        $tempList = $tempList | ForEach-Object {$_.ComputerName}
+        $FinalList = $FinalList | Where-Object {$_.ComputerName -in $tempList}
+    }
+    $FinalList | ForEach-Object {
+        Write-Host $_.ComputerName
+        [PKI.CertificateServices.CertificateAuthority]::Connect($_)
+    }
 }
