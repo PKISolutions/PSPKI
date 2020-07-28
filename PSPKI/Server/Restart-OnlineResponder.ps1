1ecec52cba6bc92959e77d56444f58c9fab3adca
Vadims Podans <vpodans@sysadmins.lv>
2020-07-28 11:51:51 +0300

    added OCSP service management actions (start/stop/restart)

---
+++ PSPKI/Server/Restart-OnlineResponder.ps1
@@ -0,0 +1,15 @@
+function Restart-OnlineResponder {
+<#
+.ExternalHelp PSPKI.Help.xml
+#>
+[CmdletBinding()]
+	param(
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+		[SysadminsLV.PKI.Management.CertificateServices.OcspResponder[]]$OnlineResponder
+	)
+	process {
+		foreach ($OCSP in $OnlineResponder) {
+			$OCSP.Restart()
+		}
+	}
+}
---
+++ PSPKI/Server/Stop-OnlineResponder.ps1
@@ -0,0 +1,15 @@
+function Stop-OnlineResponder {
+<#
+.ExternalHelp PSPKI.Help.xml
+#>
+[CmdletBinding()]
+	param(
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+		[SysadminsLV.PKI.Management.CertificateServices.OcspResponder[]]$OnlineResponder
+	)
+	process {
+		foreach ($OCSP in $OnlineResponder) {
+			$OCSP.Stop()
+		}
+	}
+}
