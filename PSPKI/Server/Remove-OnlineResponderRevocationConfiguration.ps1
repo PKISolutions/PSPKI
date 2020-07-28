5f7b5920ee89121141d77e5962c864c40e8e5a2a
Vadims Podans <vpodans@sysadmins.lv>
2020-07-28 11:12:28 +0300

    fixed copy/paste issues

--- PSPKI/Server/Remove-OnlineResponderRevocationConfiguration.ps1
+++ PSPKI/Server/Remove-OnlineResponderRevocationConfiguration.ps1
@@ -2,24 +2,26 @@ function Remove-OnlineResponderRevocationConfiguration {
 <#
 .ExternalHelp PSPKI.Help.xml
 #>
-[OutputType('SysadminsLV.PKI.Management.CertificateServices.OcspResponderRevocationConfiguration')]
+[OutputType('SysadminsLV.PKI.Management.CertificateServices.OcspResponder')]
 [CmdletBinding(DefaultParameterSetName = '__config')]
 	param (
 		[Parameter(Mandatory = $true, ParameterSetName = '__config', ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-		[SysadminsLV.PKI.Management.CertificateServices.OcspResponder[]]$RevocationConfiguration,
+		[SysadminsLV.PKI.Management.CertificateServices.OcspResponder[]]$OnlineResponder,
 		[Parameter(Mandatory = $true, ParameterSetName = '__name', ValueFromPipelineByPropertyName = $true)]
 		[string[]]$Name
 	)
 
 	process {
-		foreach ($RevConfig in $RevocationConfiguration) {
-			$OnlineResponder = Connect-OnlineResponder $RevConfig.ComputerName
-			switch ($PSCmdlet.ParameterSetName) {
-				'__config' {$OnlineResponder.RemoveRevocationConfiguration($RevConfig)}
-				'__name' {$OnlineResponder.RemoveRevocationConfiguration($Name)}
+		$OnlineResponder | ForEach-Object {
+			$RevConfigs = $OnlineResponder.GetRevocationConfigurations() | ForEach-Object {$_}
+			foreach ($RevConfig in $RevConfigs) {
+				switch ($PSCmdlet.ParameterSetName) {
+					'__config' {$OnlineResponder.RemoveRevocationConfiguration($RevConfig)}
+					'__name' {$OnlineResponder.RemoveRevocationConfiguration($Name)}
+				}
 			}
-						
-			$RevConfig
+
+			$_
 		}
 	}
 }
