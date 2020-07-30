0130e3197ecfe1f54e1c5eab4cf186db6b9a5201
Vadims Podans <vpodans@sysadmins.lv>
2020-07-30 20:54:53 +0300

    Added "-CA" parameter in separate parameter set to handle Enterprise CAs.

--- PSPKI/Server/Add-OnlineResponderRevocationConfiguration.ps1
+++ PSPKI/Server/Add-OnlineResponderRevocationConfiguration.ps1
@@ -3,18 +3,25 @@ function Add-OnlineResponderRevocationConfiguration {
 .ExternalHelp PSPKI.Help.xml
 #>
 [OutputType('SysadminsLV.PKI.Management.CertificateServices.OcspResponderRevocationConfiguration')]
-[CmdletBinding()]
+[CmdletBinding(DefaultParameterSetName = '__cert')]
 	param (
 		[Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
 		[SysadminsLV.PKI.Management.CertificateServices.OcspResponder[]]$OnlineResponder,
 		[Parameter(Mandatory = $true)]
 		[string]$Name,
-		[System.Security.Cryptography.X509Certificates.X509Certificate]$CaCertificate
+		[Parameter(Mandatory = $true, ParameterSetName = "__cert")]
+		[System.Security.Cryptography.X509Certificates.X509Certificate]$CaCertificate,
+		[Parameter(Mandatory = $true, ParameterSetName = "__ca")]
+		[Alias('CA')]
+		[PKI.CertificateServices.CertificateAuthority]$CertificationAuthority
 	)
 
 	process {
 		foreach ($Responder in $OnlineResponder) {
-			$OnlineResponder.AddRevocationConfiguration($Name, $CaCertificate)
+			switch ($PSCmdlet.ParameterSetName) {
+				"__cert" {$OnlineResponder.AddRevocationConfiguration($Name, $CaCertificate)}
+				"__ca" {$OnlineResponder.AddRevocationConfiguration($Name, $CertificationAuthority)}
+			}
 		}
 	}
 }
