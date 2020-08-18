e2542de4bd5c00f90c2683b6dbb12bcd24847254
kdhlab <>
2020-08-18 15:17:23 -0400

    Fix variable error on Unregister-ObjectIdentifier

--- PSPKI/Client/Unregister-ObjectIdentifier.ps1
+++ PSPKI/Client/Unregister-ObjectIdentifier.ps1
@@ -14,7 +14,7 @@
 	)
 	if ($Force -or $PSCmdlet.ShouldProcess(
 		$Env:COMPUTERNAME,
-		"Unregister object identifier with name: '$(Value.FriendlyName)' and value: '$($Value.Value)'"
+		"Unregister object identifier with name: '$($Value.FriendlyName)' and value: '$($Value.Value)'"
 	)) {
 		$retValue = [Security.Cryptography.Oid2]::Unregister($Value.Value,$Value.OidGroup,$UseActiveDirectory)
 		if (!$retValue) {Write-Error "An error occured while attempting to unregister specified object identifier"}
