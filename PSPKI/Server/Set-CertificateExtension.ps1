ae4c2f0483aea627eee6d148865d242ced79b4c7
Vadims Podāns <vpodans@sysadmins.lv>
2023-06-06 17:42:11 +0300

    updated result text to reflect actual action

--- PSPKI/Server/Set-CertificateExtension.ps1
+++ PSPKI/Server/Set-CertificateExtension.ps1
@@ -43,10 +43,10 @@
 				foreach ($ext in $Extension) {
 					try {
 						$oid = New-Object Security.Cryptography.Oid $ext
-						[void][SysadminsLV.Asn1Parser.Asn1Utils]::EncodeObjectIdentifier($ext)
+						[void](New-Object SysadminsLV.Asn1Parser.Universal.Asn1ObjectIdentifier $ext)
 						$CertAdmin.SetCertificateExtension($Req.ConfigString,$Req.RequestID,$oid.Value,0x1,0x2,0)
 						New-Object SysadminsLV.PKI.Utils.ServiceOperationResult 0,
-							"Extension '$([SysadminsLV.PKI.Cryptography.OidExtensions]::Format($oid, $true))' was addedd to request ID='$($Req.RequestID)'."
+							"Extension '$([SysadminsLV.PKI.Cryptography.OidExtensions]::Format($oid, $true))' was removed from request ID='$($Req.RequestID)'."
 						Write-Verbose "Extension OID='$($oid.Value)' was removed from request ID='$($Req.RequestID)'."
 					} catch {
 						throw $_
