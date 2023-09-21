188c872746079d8e1ddcbcff90ac47378bcf156b
Vadims Podāns <vpodans@sysadmins.lv>
2023-09-21 14:00:12 +0300

    completed Get-RequestArchivedKey function. Tested.

--- PSPKI/Server/Get-RequestArchivedKey.ps1
+++ PSPKI/Server/Get-RequestArchivedKey.ps1
@@ -6,22 +6,18 @@ function Get-RequestArchivedKey {
 [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
-        [SysadminsLV.PKI.Management.CertificateServices.Database.AdcsDbRow[]]$Request,
-        # this parameter is optional.
-        [System.IO.FileInfo]$OutFile
+        [SysadminsLV.PKI.Management.CertificateServices.Database.AdcsDbRow[]]$Request
     )
 
     process {
         foreach ($req in $Request) {
             $CertAdmin = New-Object -ComObject CertificateAuthority.Admin
             try {
-                $base64 = $CertAdmin.GetArchivedKey($_.ConfigString)
-                $cms = New-Object SysadminsLV.PKI.Cryptography.Pkcs.DefaultSignedPkcs7 (,$base64)
-                if ($OutFile) {
-                    Export-Binary $OutFile.FullName $cms.RawData
-                }
-
-                $cms
+                $base64 = $CertAdmin.GetArchivedKey($req.ConfigString, $req.RowID, 1)
+                $bytes = [convert]::FromBase64String($base64)
+                New-Object SysadminsLV.PKI.Cryptography.Pkcs.DefaultSignedPkcs7 (,$bytes)
+            } catch {
+                Write-Error -Exception $_.Exception
             } finally {
                 Release-COM $CertAdmin
             }
