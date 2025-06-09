f800ac4312a008e7d7f321e45a1f0a771650201a
Vadims Podāns <vpodans@sysadmins.lv>
2025-06-09 11:49:55 +0300

    fixed missing 'process' block in Import-LostCertificate

--- PSPKI/Server/Import-LostCertificate.ps1
+++ PSPKI/Server/Import-LostCertificate.ps1
@@ -19,23 +19,25 @@
         Assert-CommandRequirement $PREREQ_RSAT -ErrorAction Stop
     }
 
-    if ((Ping-ICertAdmin $CertificationAuthority.ConfigString)) {
-        $CertAdmin = New-Object -ComObject CertificateAuthority.Admin
-        try {
-            switch ($PsCmdlet.ParameterSetName) {
-                "Path" {
-                    $cert = New-Object Security.Cryptography.X509Certificates.X509Certificate2 $Path
+    process {
+        if ((Ping-ICertAdmin $CertificationAuthority.ConfigString)) {
+            $CertAdmin = New-Object -ComObject CertificateAuthority.Admin
+            try {
+                switch ($PsCmdlet.ParameterSetName) {
+                    "Path" {
+                        $cert = New-Object Security.Cryptography.X509Certificates.X509Certificate2 $Path
+                    }
+                    "X509Certificate2" {                    
+                        $cert = $Certificate
+                    }
+                    "RawData" {
+                        $cert = New-Object Security.Cryptography.X509Certificates.X509Certificate2 @(,$RawData)
+                    }
                 }
-                "X509Certificate2" {                    
-                    $cert = $Certificate
-                }
-                "RawData" {
-                    $cert = New-Object Security.Cryptography.X509Certificates.X509Certificate2 @(,$RawData)
-                }
-            }
-            $Base64 = [Convert]::ToBase64String($cert.RawData)
-            [Int64]$RowID = $CertAdmin.ImportCertificate($CertificationAuthority.ConfigString,$Base64,0x1)
-            $RowID
-        } finally { }
-    } else {Write-ErrorMessage -Source ICertAdminUnavailable $CertificationAuthority.ComputerName}
+                $Base64 = [Convert]::ToBase64String($cert.RawData)
+                [Int64]$RowID = $CertAdmin.ImportCertificate($CertificationAuthority.ConfigString,$Base64,0x1)
+                $RowID
+            } finally { }
+        } else {Write-ErrorMessage -Source ICertAdminUnavailable $CertificationAuthority.ComputerName}
+    }
 }
